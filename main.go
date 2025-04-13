1ad634420e8d81d7db9a806104282daa82dd524c
Gabriel Vincent <gabriel@vincent.es>
2025-04-13 10:33:44 -0300

    add support for intentional blank lines preservation

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ type Formatter struct {
 	source []byte
 }
 
+var lineStartPositions []int
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: crystalfmt <file.cr>")
@@ -43,49 +45,61 @@ func main() {
 		source: source,
 	}
 
+	lineStartPositions = buildLineStartPositions(source)
+
 	f.formatNode(tree.RootNode(), 0)
 
 	formatted := f.b.String()
 
-	// Overwrite original file with formatted output
-	err = os.WriteFile(filename, []byte(formatted), 0644)
-	if err != nil {
-		fmt.Printf("Failed to write file: %v\n", err)
-		os.Exit(1)
+	var shouldWrite bool
+	for _, arg := range os.Args {
+		if arg == "--write" || arg == "-w" {
+			shouldWrite = true
+			break
+		}
+	}
+
+	if shouldWrite {
+		err = os.WriteFile(filename, []byte(formatted), 0644)
+		if err != nil {
+			fmt.Printf("Failed to write file: %v\n", err)
+			os.Exit(1)
+		}
 	}
 
 	fmt.Println(string(formatted))
 }
 
-func foreachChild(node *sitter.Node, fn func(ch *sitter.Node)) {
-	for idx := range node.ChildCount() {
-		ch := node.Child(int(idx))
-		fn(ch)
-	}
-}
-
 func (f *Formatter) formatMethod(node *sitter.Node, indent int) {
-	f.b.WriteByte('\n')
+
+	prev := node.PrevSibling()
+	if prev != nil && prev.Type() != "require" {
+		f.writeByte('\n')
+	}
+
 	foreachChild(node, func(ch *sitter.Node) {
 		switch ch.Type() {
 		case "def":
 			f.writeString("def")
-			f.b.WriteByte(' ')
+			f.writeByte(' ')
 		case "identifier":
 			f.writeContent(ch)
-		case "(":
-			f.b.WriteByte('(')
-		case ")":
-			f.writeString(")\n")
+		case "(", ")":
+			f.writeContent(ch)
 		case "expressions":
-			f.b.WriteByte('\n')
+			f.writeByte('\n')
 			f.formatNode(ch, indent+INDENT_SIZE)
 		case "param_list":
 			f.formatParams(ch)
 		case "end":
-			f.b.WriteString("end")
+			f.writeString("end")
 		}
 	})
+
+	next := node.NextSibling()
+	if next != nil && next.Type() != "method_def" {
+		f.writeByte('\n')
+	}
 }
 
 func (f *Formatter) formatRequire(node *sitter.Node) {
@@ -204,18 +218,80 @@ func (f *Formatter) formatArguments(node *sitter.Node, indent int) {
 	})
 }
 
+// getAbsPosition converts a Tree-sitter Point (line, column) to an absolute byte position
+// in the source code. It requires a pre-computed array of line start positions.
+func getAbsPosition(sp sitter.Point, lineStartPositions []int) int {
+	// If we have an invalid point or empty lineStartPositions, return 0
+	if len(lineStartPositions) == 0 {
+		return 0
+	}
+
+	// Make sure we don't go out of bounds
+	if int(sp.Row) >= len(lineStartPositions) {
+		// Return the last position if the row is beyond our line count
+		return lineStartPositions[len(lineStartPositions)-1]
+	}
+
+	// Get the starting position of the line
+	lineStart := lineStartPositions[sp.Row]
+
+	// Add the column offset to get the absolute position
+	return lineStart + int(sp.Column)
+}
+
+// Helper function to build the line start positions array
+// This should be called once when loading the source code
+func buildLineStartPositions(source []byte) []int {
+	positions := []int{0} // First line always starts at position 0
+
+	for i := range source {
+		// If we find a newline character, the next line starts at i+1
+		if source[i] == '\n' {
+			positions = append(positions, i+1)
+		}
+	}
+
+	return positions
+}
+
 func (f *Formatter) formatExpressions(node *sitter.Node, indent int) {
 	foreachChild(node, func(ch *sitter.Node) {
+		// Count consecutive newlines before this node
+		pos := getAbsPosition(ch.StartPoint(), lineStartPositions) - 1
+		newlineCount := 0
+
+		// Skip backwards, counting newlines and ignoring other whitespace
+		for pos >= 0 {
+			if f.source[pos] == '\n' {
+				newlineCount++
+			} else if !isWhitespace(f.source[pos]) {
+				// If we hit a non-whitespace character, stop counting
+				break
+			}
+			pos--
+		}
+
+		// Preserve blank lines (a blank line is represented by 2 consecutive newlines)
+		if newlineCount > 1 {
+			f.writeByte('\n')
+		}
+
 		f.writeIndent(indent)
 		f.formatNode(ch, indent)
-		f.b.WriteByte('\n')
+		f.writeByte('\n')
 	})
+}
+
+// Helper function to check for whitespace
+func isWhitespace(b byte) bool {
+	return b == ' ' || b == '\t' || b == '\n' || b == '\r'
 }
 
 // Recursive function to format the syntax tree
 func (f *Formatter) formatNode(node *sitter.Node, indent int) {
 
 	// fmt.Println(strings.Repeat(" ", indent) + node.Type())
+
 	// if node.ChildCount() == 0 {
 	// 	return
 	// }
@@ -225,8 +301,8 @@ func (f *Formatter) formatNode(node *sitter.Node, indent int) {
 	// })
 	//
 	// return
-
-	fmt.Println("--- ", node.Type())
+	//
+	// fmt.Println("--- ", node.Type())
 
 	switch node.Type() {
 	case "method_def":
@@ -278,4 +354,11 @@ func (f *Formatter) writeString(str string, a ...any) {
 
 func (f *Formatter) writeByte(b byte) {
 	f.b.WriteByte(b)
+}
+
+func foreachChild(node *sitter.Node, fn func(ch *sitter.Node)) {
+	for idx := range node.ChildCount() {
+		ch := node.Child(int(idx))
+		fn(ch)
+	}
 }
