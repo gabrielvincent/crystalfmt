03c4dcf3c0b5c5df3741003982a8414304e71441
Gabriel Vincent <gabriel@vincent.es>
2025-04-17 17:38:20 -0300

    add more comments tests

--- testdata/comments_input.cr
+++ testdata/comments_input.cr
@@ -36,3 +36,39 @@ end
 
 class This < NotAMeth
 end
+
+# Multiline comments group together
+# In as many lines as yo wish
+# Like a terrible poem
+# As are all poems, really
+
+# Now I'm a lonely comment
+
+# And once again we're back to writing poetry
+# This is unadvised
+# To treat poetry as art
+
+# Multiline comments group together
+# In as many lines as yo wish
+# Like a terrible poem
+# As are all poems, really
+
+# Now I'm a lonely comment
+
+# And once again we're back to writing poetry
+# This is unadvised
+# To treat poetry as art
+
+# We can also space sections of a comment with empty comments:
+#
+# This is one space. Now let me show you two:
+#
+#
+# Cool, huh?
+
+# Inline comments in expressions
+[[1,2,3]].each do |(x, y, z)|
+x #=> 1
+y #=> 2
+z #=> 3
+end
